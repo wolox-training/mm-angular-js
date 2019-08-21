7129b135da7323304e90f6473c543dea31029122
martinmona <martin.monastirsky@wolox.com.ar>
2019-08-21 15:48:48 -0300

    update bookbyid

--- AngularJS/src/components/bookDetails/bookDetails.component.js
+++ AngularJS/src/components/bookDetails/bookDetails.component.js
@@ -3,6 +3,6 @@ const angular = require('angular');
 angular.module('app-bootstrap').component('bookDetails', {
   template: require('./bookDetails.pug')(),
   controller: ['$stateParams', 'bookService', function ($stateParams, bookService) {
-    this.book = bookService.getBooks().find(book => book.id === parseInt($stateParams.id));
+    this.book = bookService.getBookById(parseInt($stateParams.id));
   }]
 });
