08292dfa7303d8384ac14c0a19cdc512455cbf88
martinmona <martin.monastirsky@wolox.com.ar>
2019-08-21 18:27:14 -0300

    add registration form

--- AngularJS/src/components/registration/registration.component.js
+++ AngularJS/src/components/registration/registration.component.js
@@ -2,7 +2,22 @@ const angular = require('angular');
 
 angular.module('app-bootstrap').component('registration', {
   template: require('./registration.pug')(),
-  controller: [function () {
-    this.component2Phrase = 'This is registration 2';
+  controller: ['userService', function (userService) {
+    this.createUser = function() {
+      this.name = '';
+      this.lastname = '';
+      this.password = '';
+      this.email = '';
+      this.sendForm = function() {
+        if (this.name && this.lastname && this.password && this.email) {
+          const user = { name: this.name,
+            lastname: this.lastname,
+            password: this.password,
+            email: this.email };
+          userService.create(user);
+          this.transitionTo('home');
+        }
+      };
+    };
   }]
 });
---
+++ AngularJS/src/services/users.service.js
@@ -0,0 +1,12 @@
+const angular = require('angular');
+
+angular.module('app-bootstrap').factory('userService', [
+  function() {
+    var listUsers = {};
+    return {
+      getUsers: () => listUsers,
+      getUsersyId: (id) => listUsers.find(user => user.id === id),
+      create: (user) => listUsers.add(user)
+    };
+  }
+]);
