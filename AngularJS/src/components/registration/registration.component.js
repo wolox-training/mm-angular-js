d5f7c0da17d713b79c48a70ea20468827982df83
martinmona <martin.monastirsky@wolox.com.ar>
2019-08-22 15:17:19 -0300

    add validation

--- AngularJS/src/components/registration/registration.component.js
+++ AngularJS/src/components/registration/registration.component.js
@@ -2,22 +2,24 @@ const angular = require('angular');
 
 angular.module('app-bootstrap').component('registration', {
   template: require('./registration.pug')(),
-  controller: ['userService', function (userService) {
-    this.createUser = function() {
-      this.name = '';
-      this.lastname = '';
-      this.password = '';
-      this.email = '';
-      this.sendForm = function() {
-        if (this.name && this.lastname && this.password && this.email) {
-          const user = { name: this.name,
-            lastname: this.lastname,
-            password: this.password,
-            email: this.email };
-          userService.create(user);
-          this.transitionTo('home');
-        }
-      };
+  controller: ['userService', '$state', function (userService, $state) {
+    this.submitted = false;
+    this.user = {
+      name: '',
+      lastname: '',
+      password: '',
+      email: ''
+    };
+    this.error = {
+      name: false,
+      lastname: false,
+      password: false,
+      email: false
+    };
+    this.createUser = () => {
+      this.submitted = true;
+      userService.create(this.user);
+      $state.transitionTo('home');
     };
   }]
 });
--- AngularJS/src/services/users.service.js
+++ AngularJS/src/services/users.service.js
@@ -2,11 +2,11 @@ const angular = require('angular');
 
 angular.module('app-bootstrap').factory('userService', [
   function() {
-    var listUsers = {};
+    var listUsers = [];
     return {
       getUsers: () => listUsers,
       getUsersyId: (id) => listUsers.find(user => user.id === id),
-      create: (user) => listUsers.add(user)
+      create: (user) => listUsers.push(user)
     };
   }
 ]);
